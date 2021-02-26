930d41109493556897852777451ae0f0c84586d0
Aaron Ward <aaron.l.ward@gmail.com>
2021-02-26 16:55:36 -0500

    cleanup ConferenceDetail.test.js

--- src/pages/__tests__/ConferenceDetail.test.js
+++ src/pages/__tests__/ConferenceDetail.test.js
@@ -9,25 +9,6 @@ import seedConference from '../../mocks/seed/seedConference';
 describe('ConferenceDetail', () => {
   it('should render the expected Conference fields', async () => {
     // arrange
-
-    // const conference = {
-    //   id: 'idValue',
-    //   name: 'nameValue1',
-    //   slug: 'slugValue1',
-    //   location: 'locationValue1',
-    //   description: 'descriptionValue1',
-    //   path: 'pathValue1',
-    //   tags: ['tag1', 'tag2'],
-    //   socialPlatforms: [{ name: 'platform', url: 'platformUrl' }],
-    // };
-
-    const conferenceResponseMock = Promise.resolve({
-      ok: true,
-      status: 200,
-      json: () => Promise.resolve(seedConference),
-    });
-    jest.spyOn(global, 'fetch').mockImplementationOnce(() => conferenceResponseMock);
-
     const theme = createMuiTheme();
 
     const tree = (
