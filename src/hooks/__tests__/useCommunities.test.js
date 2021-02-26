ef534cc4860f585e9c2aa22a844ef9b9845be1a3
Aaron Ward <aaron.l.ward@gmail.com>
2021-02-26 13:43:04 -0500

    useCommunities.test.js unit test

--- src/hooks/__tests__/useCommunities.test.js
+++ src/hooks/__tests__/useCommunities.test.js
@@ -2,12 +2,14 @@ import { renderHook } from '@testing-library/react-hooks';
 import routes from '../../constants/routes';
 import useCommunities from '../useCommunities';
 import * as useRequest from '../useRequest';
-import seedCommunity from '../../mocks/seed/seedCommunity';
 
 describe('useCommunities', () => {
   it('should return expected from useRequest', async () => {
     // arrange
-    const community = { ...seedCommunity };
+    const community = { slug: 'slugValue' };
+    const data = {
+      communities: [community],
+    };
     const expectedCommunities = [
       {
         slug: 'slugValue',
@@ -15,11 +17,20 @@ describe('useCommunities', () => {
       },
     ];
 
+    const useRequestHook = {
+      data,
+      isLoaded: true,
+      error: 'errorValue',
+    };
+    jest.spyOn(useRequest, 'default').mockImplementation(() => useRequestHook);
+
     // act
     const { result } = renderHook(() => useCommunities());
 
     // assert
     expect(result.current.communities).toEqual(expectedCommunities);
+    expect(result.current.isLoaded).toEqual(useRequestHook.isLoaded);
+    expect(result.current.error).toEqual(useRequestHook.error);
   });
 
   it('should return expected from useRequest given data is null', async () => {
