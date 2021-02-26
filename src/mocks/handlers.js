89d7456486ab6d9b07aaf707c9271df42a5ac69b
Aaron Ward <aaron.l.ward@gmail.com>
2021-02-26 16:51:33 -0500

    msw ConferenceDetail.test.js

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,11 +1,15 @@
 import { rest } from 'msw';
 import seedCommunities from './seed/seedCommunities';
 import seedCommunity from './seed/seedCommunity';
+import seedConference from './seed/seedConference';
 
 const communitiesHandler = (req, res, ctx) => res(ctx.json(seedCommunities));
 const communityHandler = (req, res, ctx) => res(ctx.json(seedCommunity));
+const conferenceHandler = (req, res, ctx) => res(ctx.json(seedConference));
+
 // eslint-disable-next-line import/prefer-default-export
 export const handlers = [
   rest.get('*/Communities', communitiesHandler),
   rest.get(`*/Communities/:slug`, communityHandler),
+  rest.get(`*/Conferences/:conferenceId`, conferenceHandler),
 ];
--- src/pages/__tests__/ConferenceDetail.test.js
+++ src/pages/__tests__/ConferenceDetail.test.js
@@ -1,28 +1,30 @@
 import React from 'react';
-import { act, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import ConferenceDetail from '../ConferenceDetail';
 import { render } from '../../utils/test.utilitiy';
+import seedConference from '../../mocks/seed/seedConference';
 
 describe('ConferenceDetail', () => {
   it('should render the expected Conference fields', async () => {
     // arrange
-    const conference = {
-      id: 'idValue',
-      name: 'nameValue1',
-      slug: 'slugValue1',
-      location: 'locationValue1',
-      description: 'descriptionValue1',
-      path: 'pathValue1',
-      tags: ['tag1', 'tag2'],
-      socialPlatforms: [{ name: 'platform', url: 'platformUrl' }],
-    };
+
+    // const conference = {
+    //   id: 'idValue',
+    //   name: 'nameValue1',
+    //   slug: 'slugValue1',
+    //   location: 'locationValue1',
+    //   description: 'descriptionValue1',
+    //   path: 'pathValue1',
+    //   tags: ['tag1', 'tag2'],
+    //   socialPlatforms: [{ name: 'platform', url: 'platformUrl' }],
+    // };
 
     const conferenceResponseMock = Promise.resolve({
       ok: true,
       status: 200,
-      json: () => Promise.resolve(conference),
+      json: () => Promise.resolve(seedConference),
     });
     jest.spyOn(global, 'fetch').mockImplementationOnce(() => conferenceResponseMock);
 
@@ -45,17 +47,17 @@ describe('ConferenceDetail', () => {
     );
 
     // act
-    await act(async () => render(tree));
+    render(tree);
 
     // assert
-    screen.getByText(conference.name);
-    screen.getByText(conference.location);
-    screen.getByText(conference.description);
+    await screen.findByText(seedConference.name);
+    screen.getByText(seedConference.location);
+    screen.getByText(seedConference.description);
 
-    conference.socialPlatforms.forEach(platform => {
+    seedConference.socialPlatforms.forEach(platform => {
       expect(screen.getByLabelText(platform.name)).toHaveAttribute('href', platform.name.url);
     });
 
-    conference.tags.forEach(tag => screen.getByText(tag));
+    seedConference.tags.forEach(tag => screen.getByText(tag));
   });
 });
