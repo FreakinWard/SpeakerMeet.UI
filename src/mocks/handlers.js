c8692ce335c957aa511c0cec9ac6f33dcc1607b9
Aaron Ward <aaron.l.ward@gmail.com>
2021-02-26 13:43:21 -0500

    cleaned up handlers.js

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,23 +1,11 @@
 import { rest } from 'msw';
-// import seedCommunities from './seed/seedCommunities';
+import seedCommunities from './seed/seedCommunities';
 import seedCommunity from './seed/seedCommunity';
 
-// const communitiesHandler = (req, res, ctx) => res(ctx.json(seedCommunities));
+const communitiesHandler = (req, res, ctx) => res(ctx.json(seedCommunities));
 const communityHandler = (req, res, ctx) => res(ctx.json(seedCommunity));
 // eslint-disable-next-line import/prefer-default-export
 export const handlers = [
-  // rest.get('*/Communities', communitiesHandler),
+  rest.get('*/Communities', communitiesHandler),
   rest.get(`*/Communities/:slug`, communityHandler),
-  rest.post('/login', (req, res, ctx) => {
-    const { username } = req.body;
-
-    return res(
-      ctx.json({
-        id: 'f79e82e8-c34a-4dc7-a49e-9fadc0979fda',
-        username,
-        firstName: 'John',
-        lastName: 'Maverick',
-      }),
-    );
-  }),
 ];
